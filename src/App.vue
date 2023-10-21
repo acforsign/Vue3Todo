2479aa2dfbe7099a719aae3e9eccae60868cdc30
Amirhossein khanmohammadi <amirhossein-kha@jahatpardaz.ir>
2023-10-21 06:52:08 -0700

    two add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
 import {  RouterView } from 'vue-router'
-
+import  ToDoHeader from './components/ToDoHeader.vue'
 </script>
 
 <template>
   
+  <ToDoHeader> </ToDoHeader>
+ 
 
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss" >
 
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap");
+
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/ToDoHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <img src="../assets/Vue_Logo_Black.png" alt="picture" >
+                <h1>vue to do </h1>
+            </div>
+            <div class="nav-routes">
+                <router-link ></router-link>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+
+import {RouterLink} from 'vue-routrer';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
